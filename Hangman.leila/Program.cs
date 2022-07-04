859d42181498bf28fee1f9e0b11ccaf278e6e57d
Leila Amrayi <leilaamraie@yahoo.com>
2022-07-04 22:19:39 +0200

    added support of whole word guess

--- Hangman.leila/Program.cs
+++ Hangman.leila/Program.cs
@@ -28,57 +28,65 @@ namespace hangManGame
                 Console.Write(dash+" ");
             }
             
-            List<char> correctGuessedLetters = new List<char>();
-            List<char> wrongGuessedLetters = new List<char>();
+            List<String> correctGuesses = new List<String>();
+            List<String> wrongGuesses = new List<String>();
             int maxChances = 10;
             for (int guessAttempt = 1; guessAttempt <= maxChances; guessAttempt++) 
             {
                 Console.Write("\r\nChance "+ guessAttempt + "/"+ maxChances + ", Guess a letter: ");
-                Char input = Console.ReadLine().ElementAt(0);
+                String input = Console.ReadLine();
 
-                if (correctGuessedLetters.Contains(input) || wrongGuessedLetters.Contains(input))
+                if (input == null || "".Equals(input.Trim())) {
+                    Console.WriteLine("Invalid input. Please try again.");
+                    continue;
+                }
+
+                if (correctGuesses.Contains(input) || wrongGuesses.Contains(input))
                 {
                     Console.WriteLine("You guessed this letter before. Try another one!");
                     guessAttempt--; // don't count this duplicate letter
                     continue; // don't do anything and just loop again
                 }
 
+                // if user guessed the whole word in one go, set the falg to true and skip the loop
+                if (secretWord.Equals(input))
+                {
+                    userWon = true;
+                    break;
+                }
+
+                
                 if (secretWord.Contains(input)) 
                 {
-                    correctGuessedLetters.Add(input);
+                    // count the guess as correct only if it's one character
+                    if (input.Length == 1)
+                    {
+                        correctGuesses.Add(input);
+                    }
+                    else
+                    {
+                        // if it's multiple characters, then it's not a correct guess even if it's part of the secret word
+                        wrongGuesses.Add(input);
+                    }
                 }
                 else
                 {
-                    wrongGuessedLetters.Add(input);
+                    wrongGuesses.Add(input);
                 }
 
                 // show incorrect guessed letters so far
-                Console.WriteLine("Wrong guesses so far: " + string.Join(',', wrongGuessedLetters.ToArray()));
-
-                // Solution #1
-                // process the guesses and keep the results as a word. 
-                /*foreach (char guessedLetter in correctGuessedLetters)
-                {
-                    int idx = 0;
-                    while (true) {
-                        idx = secretWord.IndexOf(guessedLetter, idx);
-                        if (idx == -1) {
-                            break;
-                        }
-                        secretWordArray[idx] = guessedLetter;
-                        idx++;
-                    }
-                }*/
+                Console.WriteLine("Wrong guesses so far: " + string.Join(',', wrongGuesses.ToArray()));
 
                 // Solution #2
                 // process the guesses and keep the results as a word. 
-                foreach (char guessedLetter in correctGuessedLetters)
+                foreach (String guess in correctGuesses)
                 {
+                    char guessedChar = guess.ElementAt(0);
                     int i = 0;
                     foreach (char secretLetter in secretWord)
                     {
                         
-                        if (secretLetter == guessedLetter)
+                        if (secretLetter == guessedChar)
                         {
                             
                             secretWordArray[i] = secretLetter;
@@ -89,7 +97,7 @@ namespace hangManGame
                 }
 
                 // present the word 
-                Console.Write("Secret word: ");
+                Console.Write("Word: ");
                 foreach (char ch in secretWordArray)
                 {
                     Console.Write(ch + " ");
@@ -111,7 +119,7 @@ namespace hangManGame
             }
             else
             {
-                Console.WriteLine("\r\nSorry! Game over!");
+                Console.WriteLine("\r\nSorry! You did not win. The correct word is: \r\n"+secretWord);
             }
 
         }
